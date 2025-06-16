e3f37e5f0ad312cb639a265d72552d741e96d484
MH <150673972+mayank-hassija@users.noreply.github.com>
2025-06-16 21:04:34 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
-) 
+  <React.StrictMode>
+    <main className="h-screen bg-gray-900 text-white">
+      <App />
+    </main>
+  </React.StrictMode>
+); 
